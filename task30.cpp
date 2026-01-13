d807d2032c37a6a1034533374d5a1b42cd27a58c
Kene Ijeh <keneijeh760@gmail.com>
2026-01-13 23:59:11 +0100

    Additonal C++ Code for the day but with a function and array aswell

---
+++ task30.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+
+float calculatePower (float Voltage,  float Resistor) {
+    return (Voltage * Voltage)/Resistor;
+};
+
+
+ int main() {
+
+     float Resistor_Value[5] = {150, 220, 330, 470, 100};
+     float Voltage ;
+
+     for (int i = 0; i < 5; i ++) {
+         cout << "Enter your Voltage Value: ";
+         cin >> Voltage;
+
+         calculatePower(Voltage, Resistor_Value[i]);
+
+     }
+
+
+
+
+
+
+
+
+
+
+     return 0;
+ }
