841ba71d3bca80e1a992c5e9ca43cf9ff07e9211
Kenechukwu Justin-Ijeh <keneijeh760@gmail.com>
2026-02-06 23:13:56 +0100

    Implement DCMotor class with direction reversal

---
+++ rev.py
@@ -0,0 +1,9 @@
+class DCMotor:
+    def __init__(self):
+        self.direction = direction
+
+
+    def revere_direction(self):
+        if self.direction == 'left':
+            self.direction = 'right'
+        self.direction = 'left'
